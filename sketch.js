d012bbd5d846435e74e5223cacedb1a793ad3421
SteveWong2008 <77519023+SteveWong2008@users.noreply.github.com>
2021-04-21 08:40:37 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,116 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var backgroundImg;
+
+var bg ;
+
+function preload() {
+    getBackgroundImg();
+    console.log(hour);
+    if(hour>=04 && hour<=06){
+        bg = "sunrise1.png";
+        }
+        else if(hour>=06 && hour<=08){
+            bg = "sunrise2.png";
+        }
+        else if(hour>=8 && hour<=10){
+            bg = "sunrise3.png";
+        }
+        else if(hour>=10 && hour<=12){
+            bg = "sunrise4.png";
+        }
+        else if(hour>=14 && hour<=16){
+            bg = "sunrise5.png";
+        }
+        else if(hour>=18 && hour<=20){
+            bg = "sunrise6.png";
+        }
+        else if(hour>=20 && hour<=22){
+            bg = "sunset7.png";
+        }
+        else if(hour>=22 && hour<=23){
+            bg = "sunset8.png";
+        }
+        else if(hour>=23 && hour<=0){
+            bg = "sunset10.png";
+        }
+        else if(hour>=0 && hour<=3){
+            bg = "sunset11.png";
+        }
+        else{
+            bg = "sunset12.png"
+        }
+    
+        backgroundImg = loadImage(bg);
+        console.log(backgroundImg)
+}
+
+function setup(){
+    var canvas = createCanvas(1200,700);
+    engine = Engine.create();
+    world = engine.world;
+
+}
+
+function draw(){
+    if(background)
+
+    background(backgroundImg);
+    // add condition to check if any background image is there to add
+
+
+    Engine.update(engine);
+
+    // write code to display time in correct format here
+
+
+}
+
+async function getBackgroundImg(){
+    var response = await fetch("http://worldtimeapi.org/api/timezone/America/Los_Angeles");
+    var responseJSON = await response.json();
+    console.log(responseJSON.datetime);
+    var datetime = responseJSON.datetime 
+    var hour = datetime.slice(11,13);
+    console.log(hour);
+    if(hour>=04 && hour<=06){
+    bg = "sunrise1.png";
+    }
+    else if(hour>=06 && hour<=08){
+        bg = "sunrise2.png";
+    }
+    else if(hour>=8 && hour<=10){
+        bg = "sunrise3.png";
+    }
+    else if(hour>=10 && hour<=12){
+        bg = "sunrise4.png";
+    }
+    else if(hour>=14 && hour<=16){
+        bg = "sunrise5.png";
+    }
+    else if(hour>=18 && hour<=20){
+        bg = "sunrise6.png";
+    }
+    else if(hour>=20 && hour<=22){
+        bg = "sunset7.png";
+    }
+    else if(hour>=22 && hour<=23){
+        bg = "sunset8.png";
+    }
+    else if(hour>=23 && hour<=0){
+        bg = "sunset10.png";
+    }
+    else if(hour>=0 && hour<=3){
+        bg = "sunset11.png";
+    }
+    else{
+        bg = "sunset12.png"
+    }
+
+    backgroundImg = loadImage(bg);
+    console.log(backgroundImg)
+}
